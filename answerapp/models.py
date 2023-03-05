f7c34ab6ca70818ae676203fcd96c6fd33e83dc8
Nency129 <nencypatel129@gmail.com>
2023-03-05 18:37:40 +0530

    second ocr

--- answerapp/models.py
+++ answerapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class QuestionandAnswer(models.Model):
+        # fields of the model
+    question1=models.CharField(max_length=500)
+    question2=models.CharField(max_length=500)
+    question3=models.CharField(max_length=500)
+    question4=models.CharField(max_length=500)
+    question5=models.CharField(max_length=500)
+    answer1=models.CharField(max_length=500)
+    answer2=models.CharField(max_length=500)
+    answer3=models.CharField(max_length=500)
+    answer4=models.CharField(max_length=500)
+    answer5=models.CharField(max_length=500)
+ 
+        # renames the instances of the model
+        # with their title name
+ 
--- answerapp/views.py
+++ answerapp/views.py
@@ -4,32 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .form import SignupForm
+from .models import *
 # Create your views here.
-# def index(request):
-#     form = UserCreationForm()
-#     if request.method=='POST':
-#         username = request.POST['username']
-#         password = request.POST['password1']
-#         print(username,password)
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             username=form.cleaned_data['username']
-#             password=form.cleaned_data['password1']
-#             user = authenticate(request, username=username, password=password)
-#             login(request,user)
-            
-#             messages.add_message(request, messages.SUCCESS,
-#                                  "Logged in successfull ")
-#             return redirect('/login/')
-           
-           
-
-#         else:
-#             messages.add_message(request, messages.SUCCESS,
-#                                  "Somethingwent wrong ")
-#             form = UserCreationForm()
-#     return render(request,'index.html',{'form':form})
+import pytesseract
 def Logout(request):
     logout(request)
     return redirect("/")
@@ -37,8 +14,33 @@ def Logout(request):
 from django.contrib.auth.decorators import login_required
 @login_required(login_url='/')
 def Home(request):
+    print("hello1")
     if request.user.is_authenticated:
-        return render(request,'home.html')
+        data=QuestionandAnswer.objects.last()
+        
+        if request.method == 'POST':   
+            dplantsimage = request.POST.get('plantsimage')
+            dplantsimage1 = request.POST.get('plantsimage1')
+            dplantsimage2 = request.POST.get('plantsimage2')
+            dplantsimage3 = request.POST.get('plantsimage3')
+            dplantsimage4 = request.POST.get('plantsimage4')
+        
+            text = pytesseract.image_to_string(dplantsimage )   
+            print(text)    
+    
+            answer1=request.POST.get('answer1')
+            answer2=request.POST.get('answer2')
+            answer3=request.POST.get('answer3')
+            answer4=request.POST.get('answer4')
+            answer5=request.POST.get('answer5')
+
+           
+            
+            messages.add_message(request, messages.SUCCESS,
+                                 "successfull ")
+        return render(request,'home.html',{'data':data})
+    
+
     return redirect('login') 
     
 def signup(request):
