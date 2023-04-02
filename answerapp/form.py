0c59f0a10ec84aa9958be09237b1fff571bc0664
krupesh87 <krupeshpatel.64.d.fe@gmail.com>
2023-04-02 09:30:12 +0530

    updated

--- answerapp/form.py
+++ answerapp/form.py
@@ -11,6 +11,12 @@ class SignupForm(UserCreationForm):
     password1=forms.CharField(max_length=25,help_text='Your password can’t be too similar to your other personal information.  Your password must contain at least 8 characters.Your password can’t be a commonly used password.  Your password can’t be entirely numeric.',widget=forms.PasswordInput(attrs={
         "placeholder":"Password"
     }))
+    name=forms.CharField(max_length=25,help_text='Only alphabet',widget=forms.PasswordInput(attrs={
+        "placeholder":"Name"
+    }))
+    Rollno=forms.CharField(max_length=25,widget=forms.PasswordInput(attrs={
+        "placeholder":"roll no"
+    }))
     password2=forms.CharField(max_length=25,help_text="Enter the same password as before, for verification.",widget=forms.PasswordInput(attrs={
         "placeholder":"Confirm Password"
     }))
